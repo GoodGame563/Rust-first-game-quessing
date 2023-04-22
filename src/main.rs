e86f1c2ea590cc16e60bf9ef9fab3f37eef3625f
GoodGame563 <radiongolubin@yandex.ru>
2023-04-22 17:39:02 +0300

    add randomaizer and compare

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Угадайте число");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("Секретное число равно {}", secret_number);
     println!("Пожалуйста, введите свою догадку.");
     let mut guess  = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Не получилось прочитать строку");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Пожалуйста, наберите число!");
     println!("Вы загадали: {}", guess);
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Слишком малое число!"),
+        Ordering::Greater => println!("Слишком большое число!"),
+        Ordering::Equal => println!("Вы выиграли!"),
+    }
 }
